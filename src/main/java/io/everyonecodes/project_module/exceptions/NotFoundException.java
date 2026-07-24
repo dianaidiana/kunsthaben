cd7670e497fe388d0bcb0c72e97085016ccd59bd
Diana Carbajal <di.fayolle@gmail.com>
2026-07-24 15:55:25 +0200

    add ApiExceptionHandler and NotFoundException

---
+++ src/main/java/io/everyonecodes/project_module/exceptions/NotFoundException.java
@@ -0,0 +1,7 @@
+package io.everyonecodes.project_module.exceptions;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
