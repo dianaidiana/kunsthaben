226dc7f74fe619ab0071c6ecebb01af598ae78b6
Diana Carbajal <di.fayolle@gmail.com>
2026-07-27 15:36:29 +0200

    remove unnecessary validation from UserResponse

--- src/main/java/io/everyonecodes/project_module/users/UserResponse.java
+++ src/main/java/io/everyonecodes/project_module/users/UserResponse.java
@@ -19,22 +19,16 @@ public class UserResponse {
 
     private Long id;
 
-    @NotBlank(message = ErrorMessages.NAME_REQUIRED)
     private String name;
 
-    @Email(message = ErrorMessages.EMAIL_INVALID)
     private String email;
 
-    @URL(message = ErrorMessages.BANNER_URL_INVALID)
     private String bannerUrl;
 
-    @URL(message = ErrorMessages.AVATAR_URL_INVALID)
     private String avatarUrl;
 
-    @NotBlank(message = ErrorMessages.CITY_REQUIRED)
     private String city;
 
-    @NotBlank(message = ErrorMessages.POSTCODE_REQUIRED)
     private String postcode;
 
     private String about;
