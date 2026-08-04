3a1f9bfb4fd27582374c8609dcc4fc44f47d2fb1
Diana Carbajal <di.fayolle@gmail.com>
2026-08-04 10:59:37 +0200

    replace at Artwork soldAt with sold (boolean)

--- src/main/java/io/everyonecodes/project_module/artworks/Artwork.java
+++ src/main/java/io/everyonecodes/project_module/artworks/Artwork.java
@@ -81,8 +81,8 @@ public class Artwork {
     @Column
     private OffsetDateTime deletedAt;
 
-    @Column
-    private OffsetDateTime soldAt;
+    @Column(columnDefinition = "BOOLEAN DEFAULT false")
+    private boolean sold;
 
     @Column(columnDefinition = "BOOLEAN DEFAULT false")
     private boolean reserved;
