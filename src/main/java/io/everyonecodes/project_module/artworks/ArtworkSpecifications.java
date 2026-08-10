9e8a8e7d9c077a2d2ddbe88c3065cd501d9accfe
Diana Carbajal <di.fayolle@gmail.com>
2026-08-10 10:19:28 +0200

    fix bug in ArtworkSpecifications (passing null specification)

--- src/main/java/io/everyonecodes/project_module/artworks/ArtworkSpecifications.java
+++ src/main/java/io/everyonecodes/project_module/artworks/ArtworkSpecifications.java
@@ -9,12 +9,12 @@ public class ArtworkSpecifications {
 
     public static Specification<Artwork> matchesKeywords(String keywords) {
         if (keywords == null || keywords.isBlank()) {
-            return null;
+            return Specification.unrestricted();
         }
         return Arrays.stream(keywords.trim().split("\\s+"))
                      .map(ArtworkSpecifications::matchesKeyword)
                      .reduce(Specification::and)
-                     .orElse(null);
+                     .orElse(Specification.unrestricted());
     }
 
     private static Specification<Artwork> matchesKeyword(String word) {
@@ -26,134 +26,134 @@ public class ArtworkSpecifications {
     }
 
     public static Specification<Artwork> hasMinPrice(Double minPrice) {
-        if (minPrice == null) return null;
+        if (minPrice == null) return Specification.unrestricted();
         return (root, query, cb) -> cb.greaterThanOrEqualTo(root.get("price"),
                 minPrice);
     }
 
     public static Specification<Artwork> hasMaxPrice(Double maxPrice) {
-        if (maxPrice == null) return null;
+        if (maxPrice == null) return Specification.unrestricted();
         return (root, query, cb) -> cb.lessThanOrEqualTo(root.get("price"),
                 maxPrice);
     }
 
     public static Specification<Artwork> hasCityIn(List<String> cities) {
-        if (cities == null || cities.isEmpty()) return null;
+        if (cities == null || cities.isEmpty()) return Specification.unrestricted();
         return (root, query, cb) -> root.get("city").in(cities);
     }
 
     public static Specification<Artwork> hasPostcodeIn(List<String> postcodes) {
-        if (postcodes == null || postcodes.isEmpty()) return null;
+        if (postcodes == null || postcodes.isEmpty()) return Specification.unrestricted();
         return (root, query, cb) -> root.get("postcode").in(postcodes);
     }
 
     public static Specification<Artwork> hasCategoryIn(List<Long> categoryIds) {
-        if (categoryIds == null || categoryIds.isEmpty()) return null;
+        if (categoryIds == null || categoryIds.isEmpty()) return Specification.unrestricted();
         return (root, query, cb) -> root.get("category").get("id").in(categoryIds);
     }
 
     public static Specification<Artwork> hasMediumIn(List<Long> mediumIds) {
-        if (mediumIds == null || mediumIds.isEmpty()) return null;
+        if (mediumIds == null || mediumIds.isEmpty()) return Specification.unrestricted();
         return (root, query, cb) -> root.get("media").get("id").in(mediumIds);
     }
 
     public static Specification<Artwork> hasSupportIn(List<Long> supportIds) {
-        if (supportIds == null || supportIds.isEmpty()) return null;
+        if (supportIds == null || supportIds.isEmpty()) return Specification.unrestricted();
         return (root, query, cb) -> root.get("support").get("id").in(supportIds);
     }
 
     public static Specification<Artwork> hasMinWidth(Double minWidth) {
-        if (minWidth == null) return null;
+        if (minWidth == null) return Specification.unrestricted();
         return (root, query, cb) -> cb.greaterThanOrEqualTo(root.get("dimensions").get("width"),
                 minWidth);
     }
 
     public static Specification<Artwork> hasMaxWidth(Double maxWidth) {
-        if (maxWidth == null) return null;
+        if (maxWidth == null) return Specification.unrestricted();
         return (root, query, cb) -> cb.lessThanOrEqualTo(root.get("dimensions").get("width"),
                 maxWidth);
     }
 
     public static Specification<Artwork> hasMinHeight(Double minHeight) {
-        if (minHeight == null) return null;
+        if (minHeight == null) return Specification.unrestricted();
         return (root, query, cb) -> cb.greaterThanOrEqualTo(root.get("dimensions").get("height"),
                 minHeight);
     }
 
     public static Specification<Artwork> hasMaxHeight(Double maxHeight) {
-        if (maxHeight == null) return null;
+        if (maxHeight == null) return Specification.unrestricted();
         return (root, query, cb) -> cb.lessThanOrEqualTo(root.get("dimensions").get("height"),
                 maxHeight);
     }
 
     public static Specification<Artwork> hasMinDepth(Double minDepth) {
-        if (minDepth == null) return null;
+        if (minDepth == null) return Specification.unrestricted();
         return (root, query, cb) -> cb.greaterThanOrEqualTo(root.get("dimensions").get("depth"),
                 minDepth);
     }
 
     public static Specification<Artwork> hasMaxDepth(Double maxDepth) {
-        if (maxDepth == null) return null;
+        if (maxDepth == null) return Specification.unrestricted();
         return (root, query, cb) -> cb.lessThanOrEqualTo(root.get("dimensions").get("depth"),
                 maxDepth);
     }
 
     public static Specification<Artwork> isFramed(Boolean framed) {
-        if (framed == null) return null;
+        if (framed == null) return Specification.unrestricted();
         return (root, query, cb) -> cb.equal(root.get("frame").get("framed"),
                 framed);
     }
 
     public static Specification<Artwork> hasMinFrameWidth(Double minFrameWidth) {
-        if (minFrameWidth == null) return null;
+        if (minFrameWidth == null) return Specification.unrestricted();
         return (root, query, cb) -> cb.greaterThanOrEqualTo(root.get("frame").get("width"),
                 minFrameWidth);
     }
 
     public static Specification<Artwork> hasMaxFrameWidth(Double maxFrameWidth) {
-        if (maxFrameWidth == null) return null;
+        if (maxFrameWidth == null) return Specification.unrestricted();
         return (root, query, cb) -> cb.lessThanOrEqualTo(root.get("frame").get("width"),
                 maxFrameWidth);
     }
 
     public static Specification<Artwork> hasMinFrameHeight(Double minFrameHeight) {
-        if (minFrameHeight == null) return null;
+        if (minFrameHeight == null) return Specification.unrestricted();
         return (root, query, cb) -> cb.greaterThanOrEqualTo(root.get("frame").get("height"),
                 minFrameHeight);
     }
 
     public static Specification<Artwork> hasMaxFrameHeight(Double maxFrameHeight) {
-        if (maxFrameHeight == null) return null;
+        if (maxFrameHeight == null) return Specification.unrestricted();
         return (root, query, cb) -> cb.lessThanOrEqualTo(root.get("frame").get("height"),
                 maxFrameHeight);
     }
 
     public static Specification<Artwork> hasMinFrameDepth(Double minFrameDepth) {
-        if (minFrameDepth == null) return null;
+        if (minFrameDepth == null) return Specification.unrestricted();
         return (root, query, cb) -> cb.greaterThanOrEqualTo(root.get("frame").get("depth"),
                 minFrameDepth);
     }
 
     public static Specification<Artwork> hasMaxFrameDepth(Double maxFrameDepth) {
-        if (maxFrameDepth == null) return null;
+        if (maxFrameDepth == null) return Specification.unrestricted();
         return (root, query, cb) -> cb.lessThanOrEqualTo(root.get("frame").get("depth"),
                 maxFrameDepth);
     }
 
     public static Specification<Artwork> hasMinYear(Integer minYear) {
-        if (minYear == null) return null;
+        if (minYear == null) return Specification.unrestricted();
         return (root, query, cb) -> cb.greaterThanOrEqualTo(root.get("year"),
                 minYear);
     }
 
     public static Specification<Artwork> hasMaxYear(Integer maxYear) {
-        if (maxYear == null) return null;
+        if (maxYear == null) return Specification.unrestricted();
         return (root, query, cb) -> cb.lessThanOrEqualTo(root.get("year"),
                 maxYear);
     }
 
     public static Specification<Artwork> hasReserved(Boolean reserved) {
-        if (reserved == null) return null;
+        if (reserved == null) return Specification.unrestricted();
         return (root, query, cb) -> cb.equal(root.get("reserved"), reserved);
     }
 
