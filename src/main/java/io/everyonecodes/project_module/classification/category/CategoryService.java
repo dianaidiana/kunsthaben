f4b71d7e45118c1015e5f2a34ef92a1fe2d908d7
Diana Carbajal <di.fayolle@gmail.com>
2026-08-19 15:11:18 +0200

    add fetchCategory to CategoryService #16

--- src/main/java/io/everyonecodes/project_module/classification/category/CategoryService.java
+++ src/main/java/io/everyonecodes/project_module/classification/category/CategoryService.java
@@ -1,5 +1,7 @@
 package io.everyonecodes.project_module.classification.category;
 
+import io.everyonecodes.project_module.exceptions.ErrorMessages;
+import io.everyonecodes.project_module.exceptions.NotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,6 +22,11 @@ public class CategoryService {
 
     public Optional<Category> findById(Long id) {
         return repository.findById(id);
+    }
+
+    public Category fetchCategory(Long id) {
+        return repository.findById(id)
+                         .orElseThrow(() -> new NotFoundException(ErrorMessages.CATEGORY_NOT_FOUND));
     }
 
     public Optional<Category> findByCode(String code) {
