573107625c3d7d21e1afc0f3442e794248d99be2
Diana Carbajal <di.fayolle@gmail.com>
2026-08-19 16:30:43 +0200

    replace Page for Slice in ArtworkRepository #17

--- src/main/java/io/everyonecodes/project_module/artworks/ArtworkRepository.java
+++ src/main/java/io/everyonecodes/project_module/artworks/ArtworkRepository.java
@@ -1,7 +1,7 @@
 package io.everyonecodes.project_module.artworks;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
@@ -10,7 +10,7 @@ import java.util.Optional;
 public interface ArtworkRepository extends JpaRepository<Artwork, Long>, JpaSpecificationExecutor<Artwork> {
     Optional<Artwork> findByIdAndDeletedAtIsNull(Long id);
 
-    Page<Artwork> findAllByDeletedAtIsNullAndSold(boolean sold, Pageable pageable);
+    Slice<Artwork> findAllByDeletedAtIsNullAndSold(boolean sold, Pageable pageable);
 
-    Page<Artwork> findAllByArtistIdAndDeletedAtIsNullAndSold(Long artistId, boolean sold, Pageable pageable);
+    Slice<Artwork> findAllByArtistIdAndDeletedAtIsNullAndSold(Long artistId, boolean sold, Pageable pageable);
 }
