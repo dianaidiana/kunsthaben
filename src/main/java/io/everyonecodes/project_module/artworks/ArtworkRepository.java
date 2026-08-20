9bced79f1a5330d55472c05263aff0ded9ddeafc
Diana Carbajal <di.fayolle@gmail.com>
2026-08-20 15:57:01 +0200

    add new native @Query findIdsMatchingKeywords #11

--- src/main/java/io/everyonecodes/project_module/artworks/ArtworkRepository.java
+++ src/main/java/io/everyonecodes/project_module/artworks/ArtworkRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ArtworkRepository extends JpaRepository<Artwork, Long>, JpaSpecificationExecutor<Artwork> {
@@ -13,4 +16,8 @@ public interface ArtworkRepository extends JpaRepository<Artwork, Long>, JpaSpec
     Slice<Artwork> findAllByDeletedAtIsNullAndSold(boolean sold, Pageable pageable);
 
     Slice<Artwork> findAllByArtistIdAndDeletedAtIsNullAndSold(Long artistId, boolean sold, Pageable pageable);
+
+    @Query(value = "SELECT id FROM artwork WHERE search_vector @@ websearch_to_tsquery('english', :keywords)",
+            nativeQuery = true)
+    List<Long> findIdsMatchingKeywords(@Param("keywords") String keywords);
 }
