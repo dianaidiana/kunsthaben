ac9c6d7e9c12ff11a8b1fd4380f377a49187b751
Diana Carbajal <di.fayolle@gmail.com>
2026-08-24 10:14:11 +0200

    add new endpoint /auth/csrf #26

--- src/main/java/io/everyonecodes/project_module/auth/AuthController.java
+++ src/main/java/io/everyonecodes/project_module/auth/AuthController.java
@@ -4,6 +4,8 @@ import io.everyonecodes.project_module.auth.dto.LoginRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -19,6 +21,11 @@ public class AuthController {
     public AuthController(AuthService authService, AuthCookieService authCookieService) {
         this.authService = authService;
         this.authCookieService = authCookieService;
+    }
+
+    @GetMapping("/auth/csrf")
+    CsrfToken csrf(CsrfToken csrfToken) {
+        return csrfToken;
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
