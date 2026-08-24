dceb54f97c53e4268f73cb513013763eff53970c
Diana Carbajal <di.fayolle@gmail.com>
2026-08-24 10:12:32 +0200

    replace csrf(AbstractHttpConfigurer::disable) with csrf(CsrfConfigurer::spa) #26

--- src/main/java/io/everyonecodes/project_module/auth/config/SecurityConfig.java
+++ src/main/java/io/everyonecodes/project_module/auth/config/SecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -43,7 +43,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) {
-        http.csrf(AbstractHttpConfigurer::disable)
+        http.csrf(CsrfConfigurer::spa)
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
                     .requestMatchers(HttpMethod.POST, "/user/register").permitAll()
